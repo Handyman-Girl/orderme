c5afffaa2e0cdecf14150e7b4a160d3d608c7563
Hugo Hirsch <github@hugo-hirsch.de>
2015-07-02 11:39:19 +0200

    Remove obsolete RunWith, reformat code.

--- src/test/java/com/kata/orderme/order/DefaultCheckOutTest.java
+++ src/test/java/com/kata/orderme/order/DefaultCheckOutTest.java
@@ -10,80 +10,78 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import com.kata.orderme.price.SimplePriceManager;
 
-@RunWith(JUnit4.class)
 public class DefaultCheckOutTest {
 
-    private CheckOut checkOut;
-    
+	private CheckOut checkOut;
 
-    @Before
-    public void createFreshCheckOut() {
-        checkOut = new DefaultCheckOut(new SimplePriceManager());
-        assertNotNull(checkOut);
-    }
+	@Before
+	public void createFreshCheckOut() {
+		checkOut = new DefaultCheckOut(new SimplePriceManager());
+		assertNotNull(checkOut);
+	}
 
-    @Test
-    public void creationAndNoItemsScanned() {
-        assertThat(checkOut.getTotal(), is(0));
-    }
+	@Test
+	public void creationAndNoItemsScanned() {
+		assertThat(checkOut.getTotal(), is(0));
+	}
 
-    @Test
-    public void scanItemsOnly() {
-        checkOut.scan('A');
-        checkOut.scan('B');
-        checkOut.scan('C');
+	@Test
+	public void scanItemsOnly() {
+		checkOut.scan('A');
+		checkOut.scan('B');
+		checkOut.scan('C');
 
-        checkOut.scan('A');
-        checkOut.scan('A');
-        checkOut.scan('A');
+		checkOut.scan('A');
+		checkOut.scan('A');
+		checkOut.scan('A');
 
-        final String toString = checkOut.toString();
-        assertThat(toString, containsString("A=4"));
-        assertThat(toString, containsString("B=1"));
-        assertThat(toString, containsString("C=1"));
-        // no rules defined yet
-        assertThat(checkOut.getTotal(), is(0));
-    }
+		final String toString = checkOut.toString();
+		assertThat(toString, containsString("A=4"));
+		assertThat(toString, containsString("B=1"));
+		assertThat(toString, containsString("C=1"));
+		// no rules defined yet
+		assertThat(checkOut.getTotal(), is(0));
+	}
 
-    @Test
-    public void testTotals() {
-        final DefaultCheckOut checkOut = new DefaultCheckOut(defaultPriceRules(new SimplePriceManager()));
-        assertNotNull(checkOut);
+	@Test
+	public void testTotals() {
+		final DefaultCheckOut checkOut = new DefaultCheckOut(
+				defaultPriceRules(new SimplePriceManager()));
+		assertNotNull(checkOut);
 
-        assertThat("" + checkOut, scanFreshly(""), is(0));
-        assertThat("" + checkOut, scanFreshly("A"), is(50));
-        assertThat("" + checkOut, scanFreshly("AB"), is(80));
-        assertThat("" + checkOut, scanFreshly("CDBA"), is(115));
+		assertThat("" + checkOut, scanFreshly(""), is(0));
+		assertThat("" + checkOut, scanFreshly("A"), is(50));
+		assertThat("" + checkOut, scanFreshly("AB"), is(80));
+		assertThat("" + checkOut, scanFreshly("CDBA"), is(115));
 
-        assertThat("" + checkOut, scanFreshly("AA"), is(100));
-        assertThat("" + checkOut, scanFreshly("AAA"), is(130));
-        assertThat("" + checkOut, scanFreshly("AAAA"), is(180));
-        assertThat("" + checkOut, scanFreshly("AAAAA"), is(230));
-        assertThat("" + checkOut, scanFreshly("AAAAAA"), is(260));
-        assertThat("" + checkOut, scanFreshly("AAAAAAA"), is(310));
+		assertThat("" + checkOut, scanFreshly("AA"), is(100));
+		assertThat("" + checkOut, scanFreshly("AAA"), is(130));
+		assertThat("" + checkOut, scanFreshly("AAAA"), is(180));
+		assertThat("" + checkOut, scanFreshly("AAAAA"), is(230));
+		assertThat("" + checkOut, scanFreshly("AAAAAA"), is(260));
+		assertThat("" + checkOut, scanFreshly("AAAAAAA"), is(310));
 
-        assertThat("" + checkOut, scanFreshly("AAAB"), is(160));
-        assertThat("" + checkOut, scanFreshly("AAABB"), is(175));
-        assertThat("" + checkOut, scanFreshly("AAABBD"), is(190));
-        assertThat("" + checkOut, scanFreshly("DABABA"), is(190));
-    }
+		assertThat("" + checkOut, scanFreshly("AAAB"), is(160));
+		assertThat("" + checkOut, scanFreshly("AAABB"), is(175));
+		assertThat("" + checkOut, scanFreshly("AAABBD"), is(190));
+		assertThat("" + checkOut, scanFreshly("DABABA"), is(190));
+	}
 
-    @Test
-    public void testIncremental() {
-        final DefaultCheckOut checkOut = new DefaultCheckOut(defaultPriceRules(new SimplePriceManager()));
-        assertNotNull(checkOut);
-        
-        assertThat("" + checkOut, scanAndTotal(checkOut, ""), is(0));
-        assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(50));
-        assertThat("" + checkOut, scanAndTotal(checkOut, "B"), is(80));
-        assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(130));
-        assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(160));
-        assertThat("" + checkOut, scanAndTotal(checkOut, "B"), is(175));
-    }
+	@Test
+	public void testIncremental() {
+		final DefaultCheckOut checkOut = new DefaultCheckOut(
+				defaultPriceRules(new SimplePriceManager()));
+		assertNotNull(checkOut);
+
+		assertThat("" + checkOut, scanAndTotal(checkOut, ""), is(0));
+		assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(50));
+		assertThat("" + checkOut, scanAndTotal(checkOut, "B"), is(80));
+		assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(130));
+		assertThat("" + checkOut, scanAndTotal(checkOut, "A"), is(160));
+		assertThat("" + checkOut, scanAndTotal(checkOut, "B"), is(175));
+	}
 
 }
